f4ad3759ab98913824391feb1839f01206b9c126
Alireza Heydari <alireza97hi@gmail.com>
2017-11-18 01:23:07 +0330

    DFS and BFS added

--- graph.py
+++ graph.py
@@ -17,8 +17,37 @@ class Graph():
             # adding edge of first and second node
             self.edges[primary_node][second_node] = weight
             self.edges[second_node][primary_node] = weight
+        return None
+
+    def bfs(self,start):
+        if start is None or start not in self.nodes:
+            return None
+        visited = set()
+        queue = [start]
+        while queue:
+            current_node = queue.pop(0)
+            print(current_node)
+            if current_node not in visited:
+                visited.add(current_node)
+                queue.extend(set(self.edges[current_node].keys()) - visited)
+        return visited
+    
+    def dfs(self,start):
+        if start is None or start not in self.nodes:
+            return None
+        visited = set()
+        nodes_stack = list()
+        nodes_stack.append(start)
+        while nodes_stack:
+            current_node = nodes_stack.pop()
+            print(current_node)
+            if current_node not in visited:
+                visited.add(current_node)
+                nodes_stack.extend(set(self.edges[current_node].keys()) - visited)
+        return visited
+
+            
 g = Graph()
 g.insert('a','b',10)
-g.insert('c')
-print(g.edges)
-print(g.nodes)
+g.insert('a','c',20)
+g.insert('b','d',10)
