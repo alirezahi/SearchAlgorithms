af9ffb5305ce42fa96604290fbaa6371ab6a631f
Alireza Heydari <alireza97hi@gmail.com>
2017-11-20 03:26:29 +0330

    BFS and DFS got better and separated to two tree and graph seearch

--- Graph.py
+++ Graph.py
@@ -1,8 +1,9 @@
 from Problem import *
 class Graph():
     def __init__(self,problem):
-        self.nodes = list()
+        self.nodes = [problem.initial_state()]
         self.edges = dict()
+        self.edges[problem.initial_state()] = dict()
         self.problem = problem
 
     def insert(self,primary_node,second_node=None,weight=-1):
@@ -21,7 +22,8 @@ class Graph():
             self.edges[second_node][primary_node] = weight
         return None
 
-    def bfs(self,start):
+    def bfs_graph_search(self,start):
+        self.__init__(self.problem)
         if start is None or start not in self.nodes:
             return None
         visited = set()
@@ -38,8 +40,25 @@ class Graph():
                     self.insert(current_node,node)
                 queue.extend(set(self.edges[current_node].keys()) - visited)
         return visited
+
+    def bfs_tree_search(self, start):
+        self.__init__(self.problem)
+        if start is None or start not in self.nodes:
+            return None
+        queue = [start]
+        while queue:
+            current_node = queue.pop(0)
+            print(current_node)
+            if self.problem.goal_test(current_node):
+                print('FOUND!!!')
+                return
+            for node in self.problem.actions(current_node):
+                self.insert(current_node, node)
+            queue.extend(set(self.edges[current_node].keys()))
+        return
     
-    def dfs(self,start):
+    def dfs_graph_search(self,start):
+        self.__init__(self.problem)
         if start is None or start not in self.nodes:
             return None
         visited = set()
@@ -47,6 +66,9 @@ class Graph():
         while nodes_stack:
             current_node = nodes_stack.pop()
             print(current_node)
+            if self.problem.goal_test(current_node):
+                print('FOUND!!!')
+                return
             if current_node not in visited:
                 visited.add(current_node)
                 for node in self.problem.actions(current_node):
@@ -54,8 +76,24 @@ class Graph():
                 nodes_stack.extend(set(self.edges[current_node].keys()) - visited)
         return visited
 
+    def dfs_tree_search(self, start):
+        self.__init__(self.problem)
+        if start is None or start not in self.nodes:
+            return None
+        nodes_stack = [start]
+        while nodes_stack:
+            current_node = nodes_stack.pop()
+            print(current_node)
+            if self.problem.goal_test(current_node):
+                print('FOUND!!!')
+                return
+            for node in self.problem.actions(current_node):
+                self.insert(current_node, node)
+            nodes_stack.extend(set(self.edges[current_node].keys()))
+        return
+
 
 p = Problem()
 g = Graph(p)
-g.insert((0,0))
-g.bfs((0,0))
+g.bfs_graph_search((0,0))
+g.dfs_graph_search((0,0))
